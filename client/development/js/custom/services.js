23411c4f2687b594933c642b7958575071f29a68
lubenets_slava <vyacheslav.lubenets@maxymiser.com>
2016-07-10 23:03:36 +0300

    added chat styles, layout, start dividing chat logic into service

--- client/development/js/custom/services.js
+++ client/development/js/custom/services.js
@@ -30,4 +30,129 @@ angular.module('services', [])
                    });
                }
            }
+       })
+
+       .factory('ChatModule', function () {
+
+           return {
+
+               connection: null,
+               address   : 'ws://localhost:3000',
+
+               actionsFromServerMap: {
+                   CLIENT_CONNECTED : 'onClientConnect',
+                   CHAT_MESSAGE_SENT: 'onChatMessageSent'
+               },
+
+               clientActionsMap: {
+                   SEND_MESSAGE: 'SEND_MESSAGE'
+               },
+
+               soundsMap: {
+                   MESSAGE_ARRIVED: {
+                       id    : 'MESSAGE_ARRIVED',
+                       source: '/sounds/notify.mp3'
+                   }
+               },
+
+               cache: {
+                   sounds: {}
+               },
+
+               init: function () {
+
+                   this.cacheSounds();
+                   this.chaseOnlineUsers();
+
+                   this.connection = window.io(this.address);
+                   this.connection.on('message', this.onMessageFromServer.bind(this));
+               },
+
+               cacheSounds: function () {
+
+                   var audio;
+                   for (var soundEvent in this.soundsMap) {
+                       if (!this.soundsMap.hasOwnProperty(soundEvent)) {
+                           continue;
+                       }
+                       audio                                                     = new Audio();
+                       audio.src                                                 = this.soundsMap[soundEvent].source;
+                       this.cache.sounds[this.soundsMap[soundEvent].id] = audio;
+                   }
+               },
+
+               chaseOnlineUsers: function () {
+
+               },
+
+               sendMessageToChat: function (message) {
+
+                   this.publishAction(this.clientActionsMap.SEND_MESSAGE, message);
+               },
+
+               playSound: function (soundEvent) {
+
+                   if (!this.cache.sounds.hasOwnProperty(soundEvent.id)) {
+                       return;
+                   }
+                   this.cache.sounds[soundEvent.id].play();
+               },
+
+               /**
+                * Main proxy from server for messages
+                * @param {string} data - message and action in JSON format
+                */
+               onMessageFromServer: function (data) {
+
+                   var message;
+
+                   try {
+                       message = JSON.parse(data);
+                   } catch (e) {
+                       return;
+                   }
+
+                   if (!(message.action && message.action in this.actionsFromServerMap)) {
+                       return;
+                   }
+
+                   this[this.actionsFromServerMap[message.action]].call(this, message);
+               },
+
+               onClientConnect: function () {
+
+                   console.log('inited');
+               },
+
+               /**
+                * Triggers when message arrives from server
+                * @param {object} message - object with data and action to run
+                */
+               onChatMessageSent: function (message) {
+
+                   if (this.connection.id === message.from) {
+                       return;
+                   }
+                   this.playSound(this.soundsMap.MESSAGE_ARRIVED);
+               },
+
+               /**
+                * Emits message with some data
+                * @param {String} action, command to run with data
+                * @param {String} [data] text to send
+                */
+               publishAction: function (action, data) {
+
+                   if (action === this.clientActionsMap.SEND_MESSAGE && !data) {
+                       return;
+                   }
+
+                   var infoToSend = {
+                       action: action,
+                       data  : data,
+                       from  : this.connection.id
+                   };
+                   this.connection.emit('message', JSON.stringify(infoToSend));
+               }
+           }
        });
--- gulpfile.js
+++ gulpfile.js
@@ -3,35 +3,44 @@ var concat       = require('gulp-concat');
 var sourcemaps   = require('gulp-sourcemaps');
 var stylus       = require('gulp-stylus');
 var autoprefixer = require('gulp-autoprefixer');
+var plumber      = require('gulp-plumber');
 
 gulp.task('javascript', function () {
     return gulp.src([
                    'client/development/js/vendor/angular.js',
                    'client/development/js/vendor/angular-route.js',
+                   'client/development/js/vendor/**.*',
                    'client/development/js/custom/controllers.js',
                    'client/development/js/custom/services.js',
                    'client/development/js/custom/directives.js',
                    'client/development/js/custom/app.js',
-                   'client/development/js/custom/clientChatManager.js'
+                   'client/development/js/custom/clientChatManager.js',
+                   'client/development/js/custom/**.*'
                ])
+               .pipe(plumber())
                .pipe(sourcemaps.init())
                .pipe(concat('main.js'))
                .pipe(sourcemaps.write())
+               .pipe(plumber.stop())
                .pipe(gulp.dest('client/public/js/'));
 });
 
 gulp.task('style:vendor', function () {
 
     return gulp.src(['client/development/css/vendor/*.css'])
+               .pipe(plumber())
                .pipe(concat('vendor.css'))
+               .pipe(plumber.stop())
                .pipe(gulp.dest('client/public/styles/'));
 });
 
 gulp.task('style:custom', function () {
     return gulp.src(['client/development/css/custom/*.styl'])
+               .pipe(plumber())
                .pipe(stylus())
                .pipe(autoprefixer())
                .pipe(concat('main.css'))
+               .pipe(plumber.stop())
                .pipe(gulp.dest('client/public/styles/'));
 });
 
