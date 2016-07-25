f1eb09c8976063510140d7757b6bf5215753bc14
lubenets_slava <vyacheslav.lubenets@maxymiser.com>
2016-07-25 23:06:30 +0300

    dividing chat logic

--- client/development/js/custom/clientChatManager.js
+++ client/development/js/custom/clientChatManager.js
@@ -1,156 +1,156 @@
 ;(function () {
 
-    var clientChatManager = {
-
-        connection: null,
-        address   : 'ws://localhost:3000',
-
-        actionsFromServerMap: {
-            CLIENT_CONNECTED : 'onClientConnect',
-            CHAT_MESSAGE_SENT: 'onChatMessageSent'
-        },
-
-        clientActionsMap: {
-            SEND_MESSAGE: 'SEND_MESSAGE'
-        },
-
-        soundsMap: {
-            MESSAGE_ARRIVED: {
-                id    : 'MESSAGE_ARRIVED',
-                source: '/sounds/notify.mp3'
-            }
-        },
-
-        keysMap: {
-            ENTER_KEY: 13
-        },
-
-        cachedElements: {
-            $body       : null,
-            $chatList   : null,
-            $chatMessage: null,
-            sounds      : {}
-        },
-
-        init: function () {
-
-            this.cachedElements.$body          = jQuery('body');
-            this.cachedElements.$chatList      = this.cachedElements.$body.find('.chat');
-            this.cachedElements.$chatMessage   = this.cachedElements.$body.find('.messageText');
-            this.cachedElements.$submitMessage = this.cachedElements.$body.find('.sendMessage');
-
-            this.cacheSounds();
-            this.addListeners();
-            this.chaseOnlineUsers();
-
-            this.connection = window.io(this.address);
-            this.connection.on('message', this.onMessageFromServer.bind(this));
-        },
-
-        cacheSounds: function () {
-
-            var audio;
-            for (var soundEvent in this.soundsMap) {
-                if (!this.soundsMap.hasOwnProperty(soundEvent)) {
-                    continue;
-                }
-                audio                                                     = new Audio();
-                audio.src                                                 = this.soundsMap[soundEvent].source;
-                this.cachedElements.sounds[this.soundsMap[soundEvent].id] = audio;
-            }
-        },
-
-        addListeners: function () {
-
-            this.cachedElements.$submitMessage.on('click', this.sendMessage.bind(this));
-            this.cachedElements.$chatMessage.on('keypress', this.sendMessage.bind(this));
-        },
-
-        chaseOnlineUsers: function () {
-
-        },
-
-        sendMessage: function (event) {
-
-            var chatMessage;
-
-            if (!(event.type === 'keypress' &&
-                event.which === this.keysMap.ENTER_KEY ||
-                event.type === 'click')) {
-                return;
-            }
-
-            chatMessage = this.cachedElements.$chatMessage.val().trim();
-            this.cachedElements.$chatMessage.val('');
-            this.publishAction(this.clientActionsMap.SEND_MESSAGE, chatMessage);
-        },
-
-        playSound: function (soundEvent) {
-
-            if (!this.cachedElements.sounds.hasOwnProperty(soundEvent.id)) {
-                return;
-            }
-            this.cachedElements.sounds[soundEvent.id].play();
-        },
-
-        /**
-         * Main proxy from server for messages
-         * @param {string} data - message and action in JSON format
-         */
-        onMessageFromServer: function (data) {
-
-            var message;
-
-            try {
-                message = JSON.parse(data);
-            } catch (e) {
-                return;
-            }
-
-            if (!(message.action && message.action in this.actionsFromServerMap)) {
-                return;
-            }
-
-            this[this.actionsFromServerMap[message.action]].call(this, message);
-        },
-
-        onClientConnect: function () {
-
-            console.log('inited');
-        },
-
-        /**
-         * Triggers when message arrives from server
-         * @param {object} message - object with data and action to run
-         */
-        onChatMessageSent: function (message) {
-
-            this.cachedElements.$chatList.append('<li class="message">' + message.data + '</li>');
-            if (this.connection.id === message.from) {
-                return;
-            }
-            this.playSound(this.soundsMap.MESSAGE_ARRIVED);
-        },
-
-        /**
-         * Emits message with some data
-         * @param {String} action, command to run with data
-         * @param {String} [data] text to send
-         */
-        publishAction: function (action, data) {
-
-            if (action === this.clientActionsMap.SEND_MESSAGE && !data) {
-                return;
-            }
-
-            var infoToSend = {
-                action: action,
-                data  : data,
-                from  : this.connection.id
-            };
-            this.connection.emit('message', JSON.stringify(infoToSend));
-        }
-    };
+    //var clientChatManager = {
+    //
+    //    connection: null,
+    //    address   : 'ws://localhost:3000',
+    //
+    //    actionsFromServerMap: {
+    //        CLIENT_CONNECTED : 'onClientConnect',
+    //        CHAT_MESSAGE_SENT: 'onChatMessageSent'
+    //    },
+    //
+    //    clientActionsMap: {
+    //        SEND_MESSAGE: 'SEND_MESSAGE'
+    //    },
+    //
+    //    soundsMap: {
+    //        MESSAGE_ARRIVED: {
+    //            id    : 'MESSAGE_ARRIVED',
+    //            source: '/sounds/notify.mp3'
+    //        }
+    //    },
+    //
+    //    keysMap: {
+    //        ENTER_KEY: 13
+    //    },
+    //
+    //    cachedElements: {
+    //        $body       : null,
+    //        $chatList   : null,
+    //        $chatMessage: null,
+    //        sounds      : {}
+    //    },
+    //
+    //    init: function () {
+    //
+    //        this.cachedElements.$body          = jQuery('body');
+    //        this.cachedElements.$chatList      = this.cachedElements.$body.find('.chat');
+    //        this.cachedElements.$chatMessage   = this.cachedElements.$body.find('.messageText');
+    //        this.cachedElements.$submitMessage = this.cachedElements.$body.find('.sendMessage');
+    //
+    //        this.cacheSounds();
+    //        this.addListeners();
+    //        this.chaseOnlineUsers();
+    //
+    //        this.connection = window.io(this.address);
+    //        this.connection.on('message', this.onMessageFromServer.bind(this));
+    //    },
+    //
+    //    cacheSounds: function () {
+    //
+    //        var audio;
+    //        for (var soundEvent in this.soundsMap) {
+    //            if (!this.soundsMap.hasOwnProperty(soundEvent)) {
+    //                continue;
+    //            }
+    //            audio                                                     = new Audio();
+    //            audio.src                                                 = this.soundsMap[soundEvent].source;
+    //            this.cachedElements.sounds[this.soundsMap[soundEvent].id] = audio;
+    //        }
+    //    },
+    //
+    //    addListeners: function () {
+    //
+    //        this.cachedElements.$submitMessage.on('click', this.sendMessage.bind(this));
+    //        this.cachedElements.$chatMessage.on('keypress', this.sendMessage.bind(this));
+    //    },
+    //
+    //    chaseOnlineUsers: function () {
+    //
+    //    },
+    //
+    //    sendMessage: function (event) {
+    //
+    //        var chatMessage;
+    //
+    //        if (!(event.type === 'keypress' &&
+    //            event.which === this.keysMap.ENTER_KEY ||
+    //            event.type === 'click')) {
+    //            return;
+    //        }
+    //
+    //        chatMessage = this.cachedElements.$chatMessage.val().trim();
+    //        this.cachedElements.$chatMessage.val('');
+    //        this.publishAction(this.clientActionsMap.SEND_MESSAGE, chatMessage);
+    //    },
+    //
+    //    playSound: function (soundEvent) {
+    //
+    //        if (!this.cachedElements.sounds.hasOwnProperty(soundEvent.id)) {
+    //            return;
+    //        }
+    //        this.cachedElements.sounds[soundEvent.id].play();
+    //    },
+    //
+    //    /**
+    //     * Main proxy from server for messages
+    //     * @param {string} data - message and action in JSON format
+    //     */
+    //    onMessageFromServer: function (data) {
+    //
+    //        var message;
+    //
+    //        try {
+    //            message = JSON.parse(data);
+    //        } catch (e) {
+    //            return;
+    //        }
+    //
+    //        if (!(message.action && message.action in this.actionsFromServerMap)) {
+    //            return;
+    //        }
+    //
+    //        this[this.actionsFromServerMap[message.action]].call(this, message);
+    //    },
+    //
+    //    onClientConnect: function () {
+    //
+    //        console.log('inited');
+    //    },
+    //
+    //    /**
+    //     * Triggers when message arrives from server
+    //     * @param {object} message - object with data and action to run
+    //     */
+    //    onChatMessageSent: function (message) {
+    //
+    //        this.cachedElements.$chatList.append('<li class="message">' + message.data + '</li>');
+    //        if (this.connection.id === message.from) {
+    //            return;
+    //        }
+    //        this.playSound(this.soundsMap.MESSAGE_ARRIVED);
+    //    },
+    //
+    //    /**
+    //     * Emits message with some data
+    //     * @param {String} action, command to run with data
+    //     * @param {String} [data] text to send
+    //     */
+    //    publishAction: function (action, data) {
+    //
+    //        if (action === this.clientActionsMap.SEND_MESSAGE && !data) {
+    //            return;
+    //        }
+    //
+    //        var infoToSend = {
+    //            action: action,
+    //            data  : data,
+    //            from  : this.connection.id
+    //        };
+    //        this.connection.emit('message', JSON.stringify(infoToSend));
+    //    }
+    //};
 
 
 })();
--- client/development/js/custom/services.js
+++ client/development/js/custom/services.js
@@ -34,84 +34,130 @@ angular.module('services', [])
 
        .factory('ChatModule', function () {
 
-           //var chat = {
-           //
-           //    connection: null,
-           //    address   : 'ws://localhost:3000',
-           //
-           //    _actionsFromServerMap: {
-           //        CHAT_MESSAGE_SENT: 'onChatMessage'
-           //    },
-           //
-           //    _clientActionsMap: {
-           //        SEND_MESSAGE: 'SEND_MESSAGE'
-           //    },
-           //
-           //    _chatMessageCallbacks: [],
-           //
-           //    init: function () {
-           //
-           //        this.connection = window.io(this.address);
-           //        this.connection.on('message', this.onMessageFromServer.bind(this));
-           //    },
-           //
-           //    /**
-           //     * Main proxy from server for messages
-           //     * @param {string} data - message and action in JSON format
-           //     */
-           //    onMessageFromServer: function (data) {
-           //
-           //        var message;
-           //
-           //        try {
-           //            message = JSON.parse(data);
-           //        } catch (e) {
-           //            return;
-           //        }
-           //
-           //        if (!(message.action && message.action in this._actionsFromServerMap)) {
-           //            return;
-           //        }
-           //
-           //        this[this._actionsFromServerMap[message.action]].call(this, message);
-           //    },
-           //
-           //    /**
-           //     * Triggers when message arrives from server
-           //     * @param {object} message - object with data and action to run
-           //     */
-           //    onChatMessage: function (message) {
-           //
-           //    },
-           //
-           //    sendMessage: function (message) {
-           //
-           //        this.publishAction(this._clientActionsMap.SEND_MESSAGE, message);
-           //    },
-           //
-           //    /**
-           //     * Emits message with some data
-           //     * @param {String} action, command to run with data
-           //     * @param {String} [data] text to send
-           //     */
-           //    publishAction: function (action, data) {
-           //
-           //        if (action === this._clientActionsMap.SEND_MESSAGE && !data) {
-           //            return;
-           //        }
-           //
-           //        var infoToSend = {
-           //            action: action,
-           //            data  : data,
-           //            from  : this.connection.id
-           //        };
-           //        this.connection.emit('message', JSON.stringify(infoToSend));
-           //    }
-           //};
-           //
-           //return {
-           //    connection : chat.connection,
-           //    init       : chat.init,
-           //    sendMessage: chat.sendMessage
-           //}
+           var chat = {
+
+               connection: null,
+               address   : 'ws://localhost:3000',
+
+               _actionsFromServerMap: {
+                   CHAT_MESSAGE_SENT: 'onChatMessage',
+                   SERVER_MESSAGE   : 'onServerMessage'
+               },
+
+               _clientActionsMap: {
+                   SEND_CHAT_MESSAGE  : 'SEND_CHAT_MESSAGE',
+                   SEND_SERVER_MESSAGE: 'SEND_SERVER_MESSAGE'
+               },
+
+               _chatMessageCallbacks   : [],
+               _serverMessagesCallbacks: [],
+
+               init: function () {
+
+                   if (this.connection) {
+                       return;
+                   }
+
+                   this.connection = window.io(this.address);
+                   this.connection.on('message', this.onMessageFromServer.bind(this));
+               },
+
+               /**
+                * Main proxy from server for messages
+                * @param {string} data - message and action in JSON format
+                */
+               onMessageFromServer: function (data) {
+
+                   var message;
+
+                   try {
+                       message = JSON.parse(data);
+                   } catch (e) {
+                       return;
+                   }
+
+                   if (!(message.action && message.action in this._actionsFromServerMap)) {
+                       return;
+                   }
+
+                   this[this._actionsFromServerMap[message.action]].call(this, message);
+               },
+
+               /**
+                * @param callback {Function} callback for chat message
+                */
+               addChatMessageListener: function (callback) {
+
+                   this._chatMessageCallbacks.push(callback);
+               },
+
+               /**
+                * @param callback {Function} callback for server message
+                */
+               addServerMessageListener: function (callback) {
+
+                   this._serverMessagesCallbacks.push(callback);
+               },
+
+               /**
+                * Triggers when chat message arrives from server
+                * @param {object} message - object with data and action to run
+                */
+               onChatMessage: function (message) {
+
+                   this._chatMessageCallbacks.forEach(function (callback) {
+
+                       callback.call(undefined, message);
+                   });
+               },
+
+               /**
+                * Triggers when server message arrives from server
+                * @param {object} message - object with data and action to run
+                */
+               onServerMessage: function (message) {
+
+                   this._serverMessagesCallbacks.forEach(function (callback) {
+
+                       callback.call(undefined, message);
+                   });
+               },
+
+               sendChatMessage: function (message) {
+
+                   this.publishAction(this._clientActionsMap.SEND_CHAT_MESSAGE, message);
+               },
+
+               sendServerMessage: function (message) {
+
+                   this.publishAction(this._clientActionsMap.SEND_SERVER_MESSAGE, message);
+               },
+
+               /**
+                * Emits message with some data
+                * @param {String} action, command to run with data
+                * @param {String} [data] text to send
+                */
+               publishAction: function (action, data) {
+
+                   if (action === this._clientActionsMap.SEND_CHAT_MESSAGE && !data) {
+                       return;
+                   }
+
+                   var infoToSend = {
+                       action: action,
+                       data  : data,
+                       from  : this.connection.id
+                   };
+                   this.connection.emit('message', JSON.stringify(infoToSend));
+               }
+           };
+
+           return {
+               init                    : chat.init.bind(chat),
+               sendChatMessage         : chat.sendChatMessage.bind(chat),
+               sendServerMessage       : chat.sendServerMessage.bind(chat),
+               addChatMessageListener  : chat.addChatMessageListener.bind(chat),
+               addServerMessageListener: chat.addServerMessageListener.bind(chat)
+           }
        });
